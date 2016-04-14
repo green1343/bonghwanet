edb03ca79bae774e95c67fe4fb87b5f5a3a63af9
green1343 <sheep9414@naver.com>
2016-04-14 22:51:35 +0900

    서버 전환 작업

---
+++ Application/src/main/java/com/example/android/basicaccessibility/MyThread.java
@@ -0,0 +1,10 @@
+package com.example.android.basicaccessibility;
+
+/**
+ * Created by sheep on 2016-04-14.
+ */
+public class MyThread extends Thread {
+
+    public boolean running = true;
+    public int data = 0;
+}
