270f4a8f743ee654e7d22950d5620295a30b952d
dkdlxkwks <dkdlxkwks@naver.com>
2016-04-15 16:40:23 +0900

    재난상황 구현

--- Application/src/main/java/com/example/android/basicaccessibility/Emergencytype.java
+++ Application/src/main/java/com/example/android/basicaccessibility/Emergencytype.java
@@ -7,6 +7,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.File;
 
@@ -14,65 +17,22 @@ public class Emergencytype extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.emergencytype);
+
+		findViewById(R.id.button7).setOnClickListener(onClickButton);
 	}
 
+	Button.OnClickListener onClickButton = new View.OnClickListener() {
+		public void onClick(View v) {
 
-	public static final int REQ_FILE_SELECT = 0;
-	public static final int REQ_CAMERA_SELECT = 0;
-
-	String cameraTempFilePath;
-
-	void uploadPicture(){
-		Intent intent = new Intent();
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-		intent.setType("image/*");
-		startActivityForResult(intent, REQ_FILE_SELECT);
-	}
-
-	void uploadFile(){
-		Intent intent = new Intent();
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-		intent.setType("*/*");
-		startActivityForResult(intent, REQ_FILE_SELECT);
-	}
-
-	void uploadCameraFile(){
-		cameraTempFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp_image.jpg";
-		File imageFile = new File(cameraTempFilePath);
-		Uri imageFileUri = Uri.fromFile(imageFile);
-
-		Intent intent = new Intent();
-		intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
-		startActivityForResult(intent, REQ_CAMERA_SELECT);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-		if(data == null)
-			return;
-
-		Manager.INSTANCE.uploadFile(getPath(data.getData()));
-
-        /*if(resultCode == RESULT_OK) {
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 2;
-
-            if (requestCode == REQ_CAMERA_SELECT) {
-            }
-            else if (requestCode == REQ_FILE_SELECT) {
-            }
-        }*/
-	}
-
-	public String getPath(Uri uri) {
-		String[] projection = {MediaStore.Images.Media.DATA};
-		Cursor cursor = managedQuery(uri, projection, null, null, null);
-		startManagingCursor(cursor);
-		int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-		cursor.moveToFirst();
-		return cursor.getString(columnIndex);
-	}
+			EditText text=(EditText)findViewById(R.id.editMessage);
+			switch (v.getId()) {
+				case R.id.button7:
+					Intent intent = new Intent(getApplicationContext(), Emergencytype_firstaid.class);
+					startActivity(intent);
+					break;
+				default:
+					break;
+			}
+		}
+	};
 }
