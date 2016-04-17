ca80f97a97fe3edd2995ed6fa23a0a0b62b7a852
YooYeonHO <yyho1994@naver.com>
2016-04-17 19:24:39 +0900

    sms_show
    show
    수신 메세지 보여주는 클래스
    
    -sms_show.xml

---
+++ SMS_SHOW.java
@@ -0,0 +1,37 @@
+package com.example.android.packet;
+
+/**
+ * Created by yoo on 2016-04-17.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android.basicaccessibility.R;
+
+public class SMS_SHOW extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //XML
+        setContentView(R.layout.activity_showsms);
+
+        TextView smsDate = (TextView) findViewById(R.id.smsDate);
+        TextView originNum = (TextView) findViewById(R.id.originNum);
+        TextView originText = (TextView) findViewById(R.id.originText);
+
+        Intent smsIntent = getIntent();
+
+        String originNumber = smsIntent.getStringExtra("originNum");
+        String originDate = smsIntent.getStringExtra("smsDate");
+        String originSmsText = smsIntent.getStringExtra("originText");
+
+        originNum.setText(originNumber);
+        smsDate.setText(originDate);
+        originText.setText(originSmsText);
+    }
+}
